8914e930188273b9aa0fcf04021fe60f46df816b
Jason-Gew <jason.ge.wu@gmail.com>
2016-11-21 00:36:06 -0600

    Read/Write Plain Text/JSON/XML Configuration File

---
+++ configure.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# configure.py is for reading basic plain text, json, xml configuration file.
+# Configuration files must follow the basic format:
+# 
+# Module is produced by Jason/Ge Wu
+# Current Release on Nov/20/2016
+
+import json
+import time
+import xml
+import os
+
+class read_config:
+	
